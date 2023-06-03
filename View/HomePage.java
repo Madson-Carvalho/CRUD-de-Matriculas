37a20084de29d90ddc3e7e23e73d304552bb9e0e
Madson-Carvalho <madsonraynnon@gmail.com>
2023-06-02 22:02:53 -0300

    Ajustando método de adicionar

--- View/HomePage.java
+++ View/HomePage.java
@@ -3,24 +3,45 @@ package View;
 import javax.swing.*;
 import java.awt.*;
 
-public class HomePage extends JPanel{
+public class HomePage extends JPanel {
     public BorderLayout layout;
 
     private Header header;
+    private Table table;
 
     public HomePage() {
         setBackground(Color.decode("#8C8C8C"));
-        layout = new BorderLayout(5,5);
+        layout = new BorderLayout(5, 5);
         setLayout(layout);
         initialize();
     }
 
     private void initialize() {
         header = new Header();
-        addComponent(header);
+        addComponent(header, "north");
+
+        table = new Table();
+        addComponent(table, "center");
     }
 
-    private void addComponent(JComponent component) {
-        add(component,BorderLayout.NORTH);
+    private void addComponent(JComponent component, String position) {
+        switch (position) {
+            case "north":
+                add(component, BorderLayout.NORTH);
+                break;
+            case "south":
+                add(component, BorderLayout.SOUTH);
+                break;
+            case "east":
+                add(component, BorderLayout.EAST);
+                break;
+            case "west":
+                add(component, BorderLayout.WEST);
+                break;
+            case "center":
+                add(component,BorderLayout.CENTER);
+                break;
+        }
+
     }
 }
